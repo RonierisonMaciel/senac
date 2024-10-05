dc75bd1d314671ab2ffd97503181eafaefae78a7
ronierisonmaciel <ronierison.maciel@gmail.com>
2024-10-05 11:23:25 -0300

    Add main.py with payment processing functionality

---
+++ APS/scripts/POO/main.py
@@ -0,0 +1,8 @@
+from controller.pagamento_controller import PagamentoController
+
+def main():
+    controller = PagamentoController()
+    controller.processar_pagamento()
+
+if __name__ == "__main__":
+    main()
