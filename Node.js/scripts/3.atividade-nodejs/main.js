973d0273be00dcf0ee67522a7267f00f3a702d6c
ronierisonmaciel <ronierison.maciel@gmail.com>
2025-01-30 14:52:55 -0300

    :zap: Add math operations module and main script for basic arithmetic functions

---
+++ Node.js/scripts/3.atividade-nodejs/main.js
@@ -0,0 +1,5 @@
+const mat = require('./math');
+
+console.log("A soma de a + b é:", mat.soma(1, 2));
+console.log("A multiplicação de a + b é:", mat.multiplicacao(7, 2));
+console.log("A divisão de a + b é:", mat.divisao(6, 2));
---
+++ Node.js/scripts/3.atividade-nodejs/math.js
@@ -0,0 +1,13 @@
+function soma(a, b){
+    return a + b;
+}
+
+function multiplicacao(a, b){
+    return a * b;
+}
+
+function divisao(a, b){
+    return a / b;
+}
+
+module.exports = {soma, multiplicacao, divisao};
